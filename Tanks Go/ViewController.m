4f48f5c58d9a7f0608a71349de76d8b85b6f1005
JasonSekhon <jason_2s@hotmail.com>
2019-02-08 16:58:41 -0800

    Initial Commit

---
+++ Tanks Go/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tanks Go
+//
+//  Created by Jason Sekhon on 2019-02-08.
+//  Copyright © 2019 Jason Sekhon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Tanks Go/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tanks Go
+//
+//  Created by Jason Sekhon on 2019-02-08.
+//  Copyright © 2019 Jason Sekhon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
