87efd0f2238a7c22367976ec5f575a981c2e91be
Renz <renz@Renzs-Mac.local>
2019-02-23 16:58:30 -0800

    added missing files for panzer and floor

---
+++ Tanks Go/Models/Floor/Floor.h
@@ -0,0 +1,10 @@
+#import <GLKit/GLKit.h>
+
+#import "Vertex.h"
+
+extern const GLKVector4 Floor_Cube_001_Floor_ambient;
+extern const GLKVector4 Floor_Cube_001_Floor_diffuse;
+extern const GLKVector4 Floor_Cube_001_Floor_specular;
+extern const float Floor_Cube_001_Floor_shininess;
+extern const Vertex Floor_Cube_001_Floor_Vertices[36];
+
