5bef32c738605faf89b2e5b59b4dfe801bac6742
Renz <renz@Renzs-Mac.local>
2019-02-23 22:40:00 -0800

    Added missing tanker/floor files

---
+++ Tanks Go/Models/Tanker/Tanker.h
@@ -0,0 +1,10 @@
+#import <GLKit/GLKit.h>
+
+#import "Vertex.h"
+
+extern const GLKVector4 Tanker_Cube_002_Tanker_ambient;
+extern const GLKVector4 Tanker_Cube_002_Tanker_diffuse;
+extern const GLKVector4 Tanker_Cube_002_Tanker_specular;
+extern const float Tanker_Cube_002_Tanker_shininess;
+extern const Vertex Tanker_Cube_002_Tanker_Vertices[576];
+
