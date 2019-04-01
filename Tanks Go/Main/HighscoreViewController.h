313f8cdcef6f07a80fbda667a0a1bb893c060e90
Student <arroyomorris28@gmail.com>
2019-04-01 11:47:58 -0700

    HighscoreViewController

---
+++ Tanks Go/Main/HighscoreViewController.h
@@ -0,0 +1,14 @@
+//
+//  HighscoreViewController.h
+//  Tanks Go
+//
+//  Created by Student on 2019-04-01.
+//  Copyright © 2019 Jason Sekhon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface HighscoreViewController : UIViewController
+
+@end
---
+++ Tanks Go/Main/HighscoreViewController.m
@@ -0,0 +1,76 @@
+//
+//  HighscoreViewController.m
+//  Tanks Go
+//
+//  Created by Student on 2019-04-01.
+//  Copyright © 2019 Jason Sekhon. All rights reserved.
+//
+
+#import "HighscoreViewController.h"
+
+@interface HighscoreViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *name1;
+@property (weak, nonatomic) IBOutlet UILabel *name2;
+@property (weak, nonatomic) IBOutlet UILabel *name3;
+@property (weak, nonatomic) IBOutlet UILabel *name4;
+@property (weak, nonatomic) IBOutlet UILabel *name5;
+@property (weak, nonatomic) IBOutlet UILabel *score1;
+@property (weak, nonatomic) IBOutlet UILabel *score2;
+@property (weak, nonatomic) IBOutlet UILabel *score3;
+@property (weak, nonatomic) IBOutlet UILabel *score4;
+@property (weak, nonatomic) IBOutlet UILabel *score5;
+@property (strong, nonnull) NSArray* names;
+@property (strong, nonnull) NSArray* scores;
+@end
+
+@implementation HighscoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self createNameLabelsArray];
+    [self createScoreLabelsArray];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)createNameLabelsArray {
+    NSMutableArray *nameLabels = [NSMutableArray init];
+    [nameLabels addObject:_name1];
+    [nameLabels addObject:_name2];
+    [nameLabels addObject:_name3];
+    [nameLabels addObject:_name4];
+    [nameLabels addObject:_name5];
+    _names = [NSArray arrayWithArray:nameLabels];
+    for (int i = 0; i < _names.count; ++i) {
+        ((UILabel *)_names[i]).text = @"Lazy Person";
+    }
+};
+
+- (void)createScoreLabelsArray {
+    NSMutableArray *scoreLabels = [NSMutableArray init];
+    [scoreLabels addObject:_score1];
+    [scoreLabels addObject:_score2];
+    [scoreLabels addObject:_score3];
+    [scoreLabels addObject:_score4];
+    [scoreLabels addObject:_score5];
+    _scores = [NSArray arrayWithArray:scoreLabels];
+    for (int i = 0; i < _scores.count; ++i) {
+        ((UILabel *)_scores[i]).text = [NSString stringWithFormat:@"%f", 1000.0 - i * 100.0];
+    }
+};
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
